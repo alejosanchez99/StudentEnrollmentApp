fe95ae2f0f6d09898e1a4b09f200deb0549def9f
Alejandro Sanchez Gomez <alejosanchezgomez@gmail.com>
2025-01-03 17:06:36 -0500

    Add project files.

---
+++ StudentEnrollment.Api/Program.cs
@@ -0,0 +1,37 @@
+WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll", policy => policy.AllowAnyHeader().AllowAnyOrigin().AllowAnyMethod());
+});
+
+WebApplication app = builder.Build();
+
+app.UseHttpsRedirection();
+app.UseCors("AllowAll");
+
+
+string[] summaries =
+[
+    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+];
+
+app.MapGet("/weatherforecast", () =>
+{
+    WeatherForecast[] forecast = Enumerable.Range(1, 5).Select(index =>
+        new WeatherForecast
+        (
+            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+            Random.Shared.Next(-20, 55),
+            summaries[Random.Shared.Next(summaries.Length)]
+        ))
+        .ToArray();
+    return forecast;
+});
+
+app.Run();
+
+internal record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
+{
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+}
